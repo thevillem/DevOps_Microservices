5c0d5044c9cc2c24e5f610b97c3763dbda2f7bee
Mike Villeneuve <mvillene@cisco.com>
2020-12-18 16:54:38 -0500

    Adding Kubernetes deployment

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+export dockerpath=volto/housing_prediction:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl apply -f app_deployment.yaml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/house-predictions 8080:80
